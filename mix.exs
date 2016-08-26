5b78815093d25f5a530c1b67c596e43b0fccef59
Zac Barnes <zac.barnes89@gmail.com>
2016-08-26 13:32:45 -0500

    drop only: test requirement for Plug to git rid of compile errors

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule Explode.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :explode,
-     version: "1.1.0",
-     elixir: "~> 1.3",
-     description: description(),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     deps: deps()]
+    [
+      app: :explode,
+      version: "1.1.0",
+      elixir: "~> 1.3",
+      description: description(),
+      package: package(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -30,10 +32,12 @@ defmodule Explode.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 2.1 or ~> 3.0"},
-     {:ecto, ">= 2.0.0"},
-     {:plug, "~> 1.0", only: :test},
-     {:ex_doc, "~> 0.18", only: :dev}]
+    [
+      {:poison, "~> 2.1 or ~> 3.0"},
+      {:ecto, ">= 2.0.0"},
+      {:plug, "~> 1.0", only: :test},
+      {:ex_doc, "~> 0.18", only: :dev}
+    ]
   end
 
   defp description do
