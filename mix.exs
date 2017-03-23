e8fe34bef3789ae7318b9988c93f280190bc00fa
Powell Kinney <pkinney@gmail.com>
2017-03-23 15:01:08 -0500

    Bump to version 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Explode.Mixfile do
 
   def project do
     [app: :explode,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.2",
      description: description(),
      package: package(),
