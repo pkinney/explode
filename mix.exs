7107a7b7a4edfc56489eb55b2937f375cda9fa07
Powell Kinney <pkinney@gmail.com>
2017-03-26 09:09:00 -0500

    Bump version to 1.0.0 since elixir 1.3 is now required.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Explode.Mixfile do
 
   def project do
     [app: :explode,
-     version: "0.3.0",
+     version: "1.0.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
